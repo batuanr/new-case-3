26c5ad80610dfc19cb941039bd9159da9facd6ca
batuanr <tuanpesi@gmail.com>
2021-11-17 08:28:42 +0700

    add admin

---
+++ src/main/java/service/admin/AdminService.java
@@ -0,0 +1,88 @@
+package service.admin;
+
+import connection.ConnectionSingleton;
+import model.Admin;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminService implements IAdminService{
+    Connection connection= ConnectionSingleton.getConnection();
+    @Override
+    public List<Admin> findAllAdmin() {
+        List<Admin> adminList= new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement("select * from admin ");
+            ResultSet rs =preparedStatement.executeQuery();
+            while (rs.next()){
+                int id=rs.getInt("id");
+                String account=rs.getString("account");
+                String pass = rs.getString("pass");
+                Admin admin=new Admin(account,pass);
+                adminList.add(admin);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return adminList;
+    }
+
+    @Override
+    public void save(Admin admin) {
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement("insert into admin (account, pass) value (?,?)");
+            preparedStatement.setString(1,admin.getAccount());
+            preparedStatement.setString(2, admin.getPass());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Admin findById(int id) {
+        Admin admin=null;
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement("select *from admin where id=?");
+            preparedStatement.setInt(1,id);
+            ResultSet rs=preparedStatement.executeQuery();
+            while (rs.next()){
+                String account =rs.getString("account");
+                String pass=rs.getString("pass");
+                admin=new Admin(account,pass);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return admin;
+
+    }
+
+    @Override
+    public void update(int id, Admin admin) {
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement("update admin set account=?,pass=? where id=?;");
+            preparedStatement.setString(1,admin.getAccount());
+            preparedStatement.setString(2,admin.getPass());
+            preparedStatement.setInt(3,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement("delete from admin where id=?;");
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
