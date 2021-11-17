1e41ebaf3604fef5e5a678647f50f00f4d28d84e
truongcpqn2421998 <truongcpqn2421998@gmail.com>
2021-11-17 10:07:11 +0700

    create customer service

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,5 +1,69 @@
 package model;
 
-public class Customer {
+import java.util.List;
 
+public class Customer {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    private String pass;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, String phone, String email, String pass) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public Customer(String name, String phone, String email, String pass) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
