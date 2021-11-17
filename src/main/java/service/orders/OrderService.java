308c842fa1170ed388653cd12a2b7974ecaecbb2
nvdinh98 <dinh.kawa.98@gmail.com>
2021-11-17 12:11:47 +0700

    kawa push

--- src/main/java/service/orders/OrderService.java
+++ src/main/java/service/orders/OrderService.java
@@ -15,7 +15,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderService implements  IOrderService{
+public class OrderService implements IOrderService {
     Connection connection = ConnectionSingleton.getConnection();
     ICustomerService iCustomerService = new CustomerService();
     IProduct iProduct = new ProductService();
@@ -28,7 +28,7 @@ public class OrderService implements  IOrderService{
         try {
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT  * from orders");
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 double amount = resultSet.getDouble("amount");
                 Date order_date = resultSet.getDate("order_date");
@@ -36,7 +36,7 @@ public class OrderService implements  IOrderService{
                 Customer customer = iCustomerService.findById(customerId);
                 boolean status = resultSet.getBoolean("status");
 
-                Orders orders = new Orders(id,amount,order_date,customer,status);
+                Orders orders = new Orders(id, amount, order_date, customer, status);
                 ordersList.add(orders);
             }
         } catch (SQLException e) {
@@ -49,35 +49,60 @@ public class OrderService implements  IOrderService{
     public Orders findOrderById(int id) {
         Orders orders = null;
         try {
-            PreparedStatement preparedStatement =connection.prepareStatement("select  * from Module3.Orders where id=?;");
-            preparedStatement.setInt(1,id);
+            PreparedStatement preparedStatement = connection.prepareStatement("select  * from Module3.Orders where id=?;");
+            preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 double amount = resultSet.getDouble("amount");
                 Date order_date = resultSet.getDate("order_date");
                 int customerId = resultSet.getInt("id");
                 Customer customer = iCustomerService.findById(customerId);
                 Boolean status = resultSet.getBoolean("status");
-                orders = new Orders(id,amount,order_date,customer,status);
+                orders = new Orders(id, amount, order_date, customer, status);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-            return  orders;
+        return orders;
     }
 
     @Override
     public void save(Orders orders) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into Module3.Orders(amount, order_date, customerId, status) VALUES (?,?,?,?);");
+            preparedStatement.setDouble(1, orders.getAmount());
+            preparedStatement.setDate(2, (Date) orders.getOrder_date());
+            preparedStatement.setInt(3, orders.getCustomer().getId());
+            preparedStatement.setBoolean(4, orders.isStatus());
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteOrder(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from Module3.orders where id=?;");
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void updateOrder(int id, Orders orders) {
+        try {
+            PreparedStatement preparedStatement =connection.prepareStatement("update Module3.Orders set amount=?,order_date=?,customerId=?,status=?;");
+            preparedStatement.setDouble(1,orders.getAmount());
+            preparedStatement.setDate(2,(Date) orders.getOrder_date());
+            preparedStatement.setInt(3,orders.getCustomer().getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
