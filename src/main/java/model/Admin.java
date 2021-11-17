d7657e163e42b551b87c17006b2bbe1674eb3fcc
nvdinh98 <dinh.kawa.98@gmail.com>
2021-11-17 10:28:59 +0700

    update order

--- src/main/java/model/Admin.java
+++ src/main/java/model/Admin.java
@@ -1,4 +1,42 @@
 package model;
 
 public class Admin {
+    private int id;
+    private String account;
+    private String pass;
+
+    public Admin(String account, String pass) {
+        this.account = account;
+        this.pass = pass;
+    }
+
+    public Admin(int id, String account, String pass) {
+        this.id = id;
+        this.account = account;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
--- src/main/java/model/Orders.java
+++ src/main/java/model/Orders.java
@@ -1,4 +1,74 @@
 package model;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 public class Orders {
+    private  int id;
+    private double amount;
+    Date order_date;
+    Customer customer;
+    List<Product> productList;
+    boolean status ;
+
+    public Orders() {
+    }
+
+    public Orders(int id, double amount, Date order_date, Customer customer, List<Product> productList, boolean status) {
+        this.id = id;
+        this.amount = amount;
+        this.order_date = order_date;
+        this.customer = customer;
+        this.productList = productList;
+        this.status = status;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(Date order_date) {
+        this.order_date = order_date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
